05b293ef82b8efb8d71795a25660916d436e15e4
zelbatmi <zakari_a@icloud.com>
2020-05-14 00:40:54 +0000

    1.0

--- app/src/main/java/digiplus/ma/mymusicapp/MainActivity.java
+++ app/src/main/java/digiplus/ma/mymusicapp/MainActivity.java
@@ -19,9 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<Artiste> list_Artiste = new ArrayList<>();
-        list_Artiste.add(new Artiste(R.drawable.baseline_play_circle_outline_black_36, getString(R.string.Top1), "Line 2"));
-        list_Artiste.add(new Artiste(R.drawable.baseline_play_circle_outline_black_36, getString(R.string.Top2), "Line 4"));
-        list_Artiste.add(new Artiste(R.drawable.baseline_play_circle_outline_black_36, getString(R.string.Top3), "Line 6"));
+        list_Artiste.add(new Artiste(R.drawable.theweekend, getString(R.string.Top1), getString(R.string.Top1_1)));
+        list_Artiste.add(new Artiste(R.drawable.camila_cabello_feat_dababy, getString(R.string.Top2), getString(R.string.Top2_2)));
+        list_Artiste.add(new Artiste(R.drawable.doja_cat, getString(R.string.Top3), getString(R.string.Top3_3)));
+        list_Artiste.add(new Artiste(R.drawable.theweekend, getString(R.string.Top1), getString(R.string.Top1_1)));
+        list_Artiste.add(new Artiste(R.drawable.camila_cabello_feat_dababy, getString(R.string.Top2), getString(R.string.Top2_2)));
+        list_Artiste.add(new Artiste(R.drawable.doja_cat, getString(R.string.Top3), getString(R.string.Top3_3)));
+        list_Artiste.add(new Artiste(R.drawable.theweekend, getString(R.string.Top1), getString(R.string.Top1_1)));
+        list_Artiste.add(new Artiste(R.drawable.camila_cabello_feat_dababy, getString(R.string.Top2), getString(R.string.Top2_2)));
+        list_Artiste.add(new Artiste(R.drawable.doja_cat, getString(R.string.Top3), getString(R.string.Top3_3)));
+        list_Artiste.add(new Artiste(R.drawable.doja_cat, getString(R.string.Top3), getString(R.string.Top3_3)));
 
         mRecyclerView = findViewById(R.id.Main_RV);
         mRecyclerView.setHasFixedSize(true);
